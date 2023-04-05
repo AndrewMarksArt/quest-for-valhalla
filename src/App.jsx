3b9c396f2657e2cb0349a20b94ab835b8fe03830
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-04-04 20:42:16 -0500

    removed lock screen orientation since it was broken

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import useLockOrientation from "./useLockOrientation";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
-    // Lock the screen orientation to portrait mode
-    useLockOrientation('portrait');
 
     return (
         <>
