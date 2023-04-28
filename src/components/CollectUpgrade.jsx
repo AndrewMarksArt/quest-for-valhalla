e103e4528023ffc17387317fae9904a67175ef75
AndrewMarksArt <andrew.colin.marks@gmail.com>
2023-04-28 15:37:59 -0500

    updated collect component styles for different screen sizes and added new image for smaller screens.

--- src/components/CollectUpgrade.jsx
+++ src/components/CollectUpgrade.jsx
@@ -8,6 +8,7 @@ export default function CollectUpgrade() {
                 Quests can be collected & upgraded into rarer high level quests which will give players much better rewards!
             </h2>
             <img src="../img/level_up_goblins.png" className="collect-level-up" />
+            <img src="../img/level_up_mobile.png" className="collect-level-up-mobile" />
             <div className="collect-bullets">
                 <h2 className="collect-bullet">Join us on <a href="https://www.discord.gg/battle-warriors" target={"_blank"} className="collect-link">Discord</a> to learn more about all the different Quests, how to collect, upgrade, & all about rarities.</h2>
                 <h2 className="collect-bullet">Sign up for <a href="#" className="collect-link">early access</a> to exclussive updates & win prizes.</h2>
